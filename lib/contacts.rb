54e83cd8573878a39d63884bba6caecc5663a6ec
Garth Lilly <garff247@gmail.com>
2019-04-22 02:54:46 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,6 +15,7 @@ require 'pry'
   # }
 
 def remove_strawberry(contacts)
-  contacts[:"Freddy Mercury"][:favorite_ice_cream_flavors].pop
-  return contacts
+  contacts.each do |person, contact_details_hash|
+    binding.pry
+  end
 end
